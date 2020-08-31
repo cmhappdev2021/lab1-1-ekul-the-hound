fba7aaa9ee2818ccd6fb47dd86e661911a8861f4
ekulthehound <lukebrunson8@gmail.com>
2020-08-31 18:25:24 +0000

    I added music lyrics

--- main.swift
+++ main.swift
@@ -4,7 +4,7 @@ print("I have learned the following:")
 print("What features make Swift a modern and safe language")
 print("How to use the Swift REPL in Terminal")
 print("How to use playgrounds to make writing Swift fun and simple")
-
+print("Circle of life. From the day we arrive on the planet And blinking, step into the sun There's more to be seen than can ever be seen More to do than can ever be done Some say, Eat or be eaten Some say, Live and let live But all are agreed as they join the stampede You should never take more than you give In the circle of life")
 // Now print your own phrases to the console. Pick one of your favorite songs. Use your knowledge of the print function to display the song title and artist.
 
 
